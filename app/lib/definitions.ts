490db79ed825d74d8c1e0cb5eae85aac09945f15
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2024-01-21 18:02:19 +0530

    Add type definitions

---
+++ app/lib/definitions.ts
@@ -0,0 +1,18 @@
+// This file contains type definitions for your data.
+// It describes the shape of the data, and what data type each property should accept.
+// For simplicity of teaching, we're manually defining these types.
+// However, these types are generated automatically if you're using an ORM such as Prisma.
+
+export type Question = {
+  id: number;
+  text: string;
+  code: string;
+  options: [
+    { id: string; text: string },
+    { id: string; text: string },
+    { id: string; text: string },
+    { id: string; text: string },
+  ];
+  correctOption: string;
+  explanation: string;
+}
